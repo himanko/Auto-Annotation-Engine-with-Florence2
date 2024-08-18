daf3cadb25951f0073b7cc57b697e383fd1652bb
himanko <himankoboruah@gmail.com>
2024-08-19 01:50:24 +0530

    Project added

---
+++ template.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "Auto-Annotation-Engine-with-Florence2"
+
+list_of_files = [
+    ".github/workflow/.gitkeep",
+    "config/config.yaml",
+    "artifact/",
+    "notebooks/data_collection.ipynb",
+    "requirements.txt",
+    "setup.py",
+    "docs/evaluation_report.md"
+]
+
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+                     
+                     
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty File: {filepath}")
+        
+            
+    else:
+        logging.info(f"{filename} is already exist.")
+                    
